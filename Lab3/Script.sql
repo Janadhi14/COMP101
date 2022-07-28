7ab63c8e4db7aac8abd19287a155730cfca3f5b6
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-28 23:31:37 +1200

    added measurement and type of measurement sql

--- Lab3/Script.sql
+++ Lab3/Script.sql
@@ -1,9 +1,8 @@
 
-
 drop table Scientist;
 -- here we are deleting the table beore begining a new one 
 
--- now we need ot create a scientist table 
+-- now we need to create a scientist table 
 create table Scientist(
 	Scientist_Num varchar(6),
 	Surname varchar(50),
@@ -11,34 +10,45 @@ create table Scientist(
 	Email varchar(50),
 	Mobile_Phone varchar(15)
 ); 
+
+
 -- now we need to insert into scientist some information
- insert into Scientist (Scientist_Num, Surname, Other_Names, Email, Mobile_Phone) values(123, 'Hankins', 'Stephanie', 'steph@mail.com', '021 123 456');
+insert into Scientist (Scientist_Num, Surname, Other_Names, Email, Mobile_Phone) 
+values(123, 'Hankins', 'Stephanie', 'steph@mail.com', '021 123 456');
+-- we are inserting the following values in this order 
 
 select * from Scientist;
 
 -- creating the sample table
 
 create table Sample(
-	siteID varchar(10),
+	Site_ID varchar(10),
 	Recorded_On varchar(50),
 	Scientist_Num varchar (10),
-	comments varchar (50)
+	comments varchar (50),
 );
 
-select * from Sample;
-
+-- creating a table for site 
 create table Site(
-	Site_ID varchar() not null unique,
-	region varchar(50) not null,
-	Description varchar(50) not null,
-	Latitude numeric(50) not null ,
-	Longitude numeric(50) not null,
-	Catchment_Area numeric(50,3) not null,
-	Catchment_Height numeric(10,3) not null,
-	Altitude numeric (5,3) 
-	
-	
+	Site_ID varchar(10),
+	region varchar(50),
+	Site_Description varchar(50),
+	Latitude varchar(50),
+	Longitude varchar(50),
+	Catchment_Area numeric(50,3),
+	Catchment_Height numeric(10,3),
+	Altitude numeric (1000,3) 
+
+);
+
+CREATE TABLE Measurement(
+	Site_I varchar(10),
+	Measurement_name varchar(50),
+	Units varchar(5)
 );
 
 
-
+CREATE TABLE Measurement_Type(
+	Measurement_name varchar(50),
+	Units varchar(5)
+);
