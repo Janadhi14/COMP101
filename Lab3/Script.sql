9391530ba2690f65e71f1c15f81bd79d70e365f2
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-28 23:45:34 +1200

    added drop table statements

--- Lab3/Script.sql
+++ Lab3/Script.sql
@@ -1,9 +1,9 @@
 
-drop table Scientist;
+DROP TABLE Scientist;
 -- here we are deleting the table beore begining a new one 
 
 -- now we need to create a scientist table 
-create table Scientist(
+CREATE TABLE Scientist(
 	Scientist_Num varchar(6),
 	Surname varchar(50),
 	Other_Names varchar(50),
@@ -13,23 +13,26 @@ create table Scientist(
 
 
 -- now we need to insert into scientist some information
-insert into Scientist (Scientist_Num, Surname, Other_Names, Email, Mobile_Phone) 
-values(123, 'Hankins', 'Stephanie', 'steph@mail.com', '021 123 456');
+INSERT INTO Scientist (Scientist_Num, Surname, Other_Names, Email, Mobile_Phone) 
+VALUES(123, 'Hankins', 'Stephanie', 'steph@mail.com', '021 123 456');
 -- we are inserting the following values in this order 
 
 select * from Scientist;
 
--- creating the sample table
 
-create table Sample(
+DROP TABLE Sample;
+-- creating the sample table
+CREATE TABLE Sample(
 	Site_ID varchar(10),
 	Recorded_On varchar(50),
 	Scientist_Num varchar (10),
 	comments varchar (50),
 );
 
+
+DROP TABLE Site;
 -- creating a table for site 
-create table Site(
+CREATE TABLE Site(
 	Site_ID varchar(10),
 	region varchar(50),
 	Site_Description varchar(50),
@@ -40,14 +43,17 @@ create table Site(
 	Altitude numeric (1000,3) 
 
 );
+DROP TABLE Measurement;
 
+-- creating a table for measurement 
 CREATE TABLE Measurement(
 	Site_I varchar(10),
 	Measurement_name varchar(50),
 	Units varchar(5)
 );
 
-
+DROP TABLE Measurement_Type;
+-- creating a table for measurement type 
 CREATE TABLE Measurement_Type(
 	Measurement_name varchar(50),
 	Units varchar(5)
