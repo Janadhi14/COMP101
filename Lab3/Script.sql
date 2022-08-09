884c7a689b5b64c19327f1591aee660697c1ff57
Janadhi Dissanayake <janadhi14@gmail.com>
2022-08-09 13:26:17 +1200

    done

--- Lab3/Script.sql
+++ Lab3/Script.sql
@@ -65,4 +65,26 @@ CREATE TABLE Site(
 
 -- viewing the Site table 
 SELECT * FROM Site
+INSERT INTO site VALUES('Dunedin','DN3','Mataura at Seaward Down', '168.7971922','-46.38953052','NULL','NULL','NULL');
 
+-- we want to not insert a whole bunch of data 
+INSERT INTO Site VALUES('AX1','Alexandra','Clutha at Luggate Br.', '169.2806558','-44.7290927',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('AX2','Alexandra','Kawarau at Chards', '168.8687202','-45.00636197',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('AX3','Alexandra','Shotover at Bowens Peak', '168.7150762','-44.99000262',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('AX4','Alexandra','Clutha at Millers Flat', '169.4100026','-45.66475459',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN1','Dunedin','Clutha at Balclutha', '169.7478945','-46.23731498',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN2','Dunedin','Mataura at Parawa', '168.5612716','-45.55826078',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN3','Dunedin','Mataura at Seaward Down', '168.7971922','-46.38953052',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN4','Dunedin','Monowai below Gates', '167.5343115','-45.80501481',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN5','Dunedin','Oreti at Lumsden', '168.4281482','-45.7162229',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN6','Dunedin','Oreti at Riverton HW Br.', '168.2659715','-46.3263175',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN7','Dunedin','Sutton at SH87', '170.0941149','-45.59619431',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN8','Dunedin','Taieri at Outram', '170.2439249','-45.8464209',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN9','Dunedin','Taieri at Tiroiti', '170.2728542','-45.25656919',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('DN10','Dunedin','Waiau at Tuatapere', '167.6858503','-46.13027339',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('TK1','Tekapo','Opihi at Grassy Banks', '171.3241622','-44.26803263','2373','542','4');
+INSERT INTO Site VALUES('TK2','Tekapo','Opihi at Rockwood', '170.941712','-44.16820894','406','627','180');
+INSERT INTO Site VALUES('TK3','Tekapo','Opuha at Skipton Br.', '170.9797435','-44.07888926','458','1020','238');
+INSERT INTO Site VALUES('TK4','Tekapo','Waitaki at Kurow', '170.4512012','-44.7005591','9741','1047','250');
+INSERT INTO Site VALUES('TK5','Tekapo','Hakatakamea above MH Br.', '170.4905505','-44.72479014','896','695','198');
+INSERT INTO Site VALUES('TK6','Tekapo','Waitaki at SH1 Br.', '171.1003766','-44.92715122','11909','956','5');
