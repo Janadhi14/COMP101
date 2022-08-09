4debc75bb522042ca4487d05b2cffb48000a85af
Janadhi Dissanayake <janadhi14@gmail.com>
2022-08-09 13:26:08 +1200

    some Changes

--- Lab4/lab4.sql
+++ Lab4/lab4.sql
@@ -1,9 +1,8 @@
 DROP TABLE Sample_Info;
 DROP TABLE Site;
 DROP TABLE Scientist;
-
-
-
+DROP TABLE Measurement;
+DROP TABLE MeasurementType;
 
 
 -- here we are deleting the table beore begining a new one 
@@ -37,10 +36,11 @@ CREATE TABLE Sample_Info(
 	Site_ID varchar(10) not null,
 	Comments_On varchar (50),
 	CONSTRAINT Sample_Info_PK PRIMARY KEY (recordedOn, Site_ID), 
-	CONSTRAINT Sample_Info_Scientist_FK FOREIGN KEY (Scientist_ID)
+	CONSTRAINT Sample_Info_Scientist_PK FOREIGN KEY (Scientist_ID)
 		REFERENCES Scientist,
 	CONSTRAINT recordedOn CHECK (extract(year from recordedOn)> 2015)			-- checking that the recorded date is later than 2015 
 );
+
 
 
 
@@ -55,14 +55,15 @@ CREATE TABLE Site(
 	Site_Description varchar(50) not null,
 	Latitude numeric(11,8) not null,
 	Longitude numeric(11,8) not null ,
-	Catchment_Area numeric(4),
-	Catchment_Height numeric(4),
-	Altitude numeric (4),
+	Catchment_Area numeric(5),
+	Catchment_Height numeric(5),
+	Altitude numeric (5),
 	CONSTRAINT PK_Site PRIMARY KEY(Site_ID),
 	CONSTRAINT Region check (Region in ('Dunedin', 'Alexandra', 'Tekapo')),
 	CONSTRAINT Altitude check (Altitude BETWEEN -10 AND 4000)
 );
 
+<<<<<<< HEAD
 -- inserting data 
 
 INSERT INTO Site VALUES('AX1','Alexandra','Clutha at Luggate Br.', '169.2806558','-44.7290927',NULL,NULL,NULL);
@@ -123,9 +124,9 @@ CREATE TABLE Site(
 	Site_Description varchar(50) not null,
 	Latitude numeric(11,8) not null,
 	Longitude numeric(11,8) not null ,
-	Catchment_Area numeric(6),
-	Catchment_Height numeric(6),
-	Altitude numeric (6),
+	Catchment_Area numeric(4),
+	Catchment_Height numeric(4),
+	Altitude numeric (4),
 	CONSTRAINT PK_Site PRIMARY KEY(Site_ID),
 	CONSTRAINT Region check (Region in ('Dunedin', 'Alexandra', 'Tekapo')),
 	CONSTRAINT Altitude check (Altitude BETWEEN -10 AND 40000)
@@ -134,8 +135,8 @@ CREATE TABLE Site(
 -- inserting data 
 
 INSERT INTO Site VALUES('AX1','Alexandra','Clutha at Luggate Br.', '169.2806558','-44.7290927',NULL,NULL,NULL) ;
-INSERT INTO Site VALUES('AX2','Alexandra','Kawarau at Chards', '168.8687202','-45.00636197',NULL,NULL,NULL);
-INSERT INTO Site VALUES('AX3','Alexandra','Shotover at Bowens Peak', '168.7150762','-44.99000262',NULL,NULL,NULL);
+INSERT INTO Site VALUES('AX2','Alexandra','Kawarau at Chards', '168.8687202','-45.00636197',NULL,NULL,NULL) ;
+INSERT INTO Site VALUES('AX3','Alexandra','Shotover at Bowens Peak', '168.7150762','-44.99000262',NULL,NULL,NULL) ;
 INSERT INTO Site VALUES('AX4','Alexandra','Clutha at Millers Flat', '169.4100026','-45.66475459',NULL,NULL,NULL) ;
 INSERT INTO Site VALUES('DN1','Dunedin','Clutha at Balclutha', '169.7478945','-46.23731498',NULL,NULL,NULL) ;
 INSERT INTO Site VALUES('DN2','Dunedin','Mataura at Parawa', '168.5612716','-45.55826078',NULL,NULL,NULL) ;
@@ -155,17 +156,6 @@ INSERT INTO Site VALUES('TK5','Tekapo','Hakatakamea above MH Br.', '170.4905505'
 INSERT INTO Site VALUES('TK6','Tekapo','Waitaki at SH1 Br.', '171.1003766','-44.92715122','11909','956','5');
 
 select * from Site;
-
-
-
-
-
-
-
-
-select * from Site;
-
-
 
 
 
